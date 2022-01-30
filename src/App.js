a5a80bbdbee01f285bdd813dfe50e8c8b42c8dd2
David-mwendwa <klatzdavie19@gmail.com>
2022-01-30 07:55:40 +0300

    add header and feedback components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-function App() {
-  
+import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
+function App() {
   return (
-    <div className='container'>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackItem />
+      </div>
+    </>
   );
 }
 
