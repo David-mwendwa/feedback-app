83926e805d88159c74762015d2c50074bb9d97db
David-mwendwa <klatzdavie19@gmail.com>
2022-02-05 11:52:09 +0300

    add feedback update functionality

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,16 +1,23 @@
-import {createContext, useState} from 'react'
+import {createContext, useContext, useState} from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id:1,
-      text: 'This item is from context',
-      rating: 10
-    }
-  ])
+  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false })
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
+  const updateFeedback = (id, updateItem) => {
+    setFeedback(feedback.map(item => item.id === id ? {...item, ...updateItem} : item))
+  }
 
   const addFeedback = (newFeedback) => {
     newFeedback = { ...newFeedback, id: uuidv4() };
@@ -23,7 +30,16 @@ export const FeedbackProvider = ({children}) => {
     }
   };
 
-  return <FeedbackContext.Provider value={{feedback, deleteFeedback, addFeedback}}>
+  return <FeedbackContext.Provider 
+    value={{
+      feedback,
+      feedbackEdit, 
+      deleteFeedback, 
+      addFeedback, 
+      editFeedback,
+      updateFeedback
+    }}
+  >
     {children}
   </FeedbackContext.Provider>
 }
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,17 +2,17 @@ const FeedbackData = [
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'This is feedback item one',
   },
   {
     id: 2,
     rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'This is feedback item two',
   },
   {
     id: 3,
     rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'This is feedback item three',
   },
 ];
 
