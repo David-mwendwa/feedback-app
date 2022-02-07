97603fecf82fa4b0e2958d6e534dc5bfd5301382
David-mwendwa <klatzdavie19@gmail.com>
2022-02-07 08:10:53 +0300

    add update & delete from JSON-Server functions

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -27,8 +27,16 @@ export const FeedbackProvider = ({children}) => {
     setIsLoading(false)
   }
 
-  const updateFeedback = (id, updateItem) => {
-    setFeedback(feedback.map(item => item.id === id ? {...item, ...updateItem} : item))
+  const updateFeedback = async (id, updateItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updateItem)
+    })
+    const data = await response.json()
+    setFeedback(feedback.map(item => item.id === id ? {...item, ...data} : item))
   }
 
   const addFeedback = async (newFeedback) => {
@@ -43,8 +51,9 @@ export const FeedbackProvider = ({children}) => {
     setFeedback([data, ...feedback]);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
